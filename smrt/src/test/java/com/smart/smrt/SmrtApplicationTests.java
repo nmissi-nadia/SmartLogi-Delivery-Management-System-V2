3a58154db678e81957f86a12b077ca516fd497e3
nmissi-nadia <nmissinadia@gmail.com>
2025-10-27 11:44:13 +0100

    initialisation dus projet

---
+++ smrt/src/test/java/com/smart/smrt/SmrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.smrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmrtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
